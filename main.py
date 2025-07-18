4bb9ac7f57c68c536fd2842a9856d7ea5ec4ffa7
Syddraf88 <acid47132@gmail.com>
2025-07-18 03:26:54 -0500

    character count function added

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from stats import word_count
+'''
+this script is dependant on functions in the stat.py file and must be imported at the top of the script.
+to use these functions you must define the book_to_analyze variable in the main function.
+and the book must be stored in a relative path that matches "../bookbot/books/"
+'''
+
+from stats import word_count, char_count
 
 def main():
     
     book_to_analyze = "frankenstein.txt"
     num_words_found = word_count(book_to_analyze)
+    character_count = char_count(book_to_analyze)
     print(F"{num_words_found} words found in the document")
+    print(f"{character_count}")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,12 +1,27 @@
-BOOK_DIR_PATH = "../bookbot/books/"
-def get_book_text(book_name):
+'''
+this file contains all of the functions related to data analasys of a selected book
+
+'''
+BOOK_DIR_PATH = "../bookbot/books/" #Global Variable to books folder
+def get_book_text(book_name): #Book file handler, Reads contents to ""
     
     file_path = BOOK_DIR_PATH + book_name
     with open(file_path) as f:
         contents = f.read()
         return contents
 
-def word_count(book_name):
+def word_count(book_name): #Counts all words in file, Dependent on get_book_text to handle file
     book_text = get_book_text(book_name)
     words = book_text.split()
     return len(words)
+
+def char_count(book_name): #Counts total occurances of each character in the file
+    book_text = get_book_text(book_name)
+    lowercase_book_text = book_text.lower()
+    characters = {}
+    for character in lowercase_book_text:
+        if character not in characters:
+            characters[character] = 1
+        else:
+            characters[character] += 1
+    return characters
