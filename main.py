cbe4d96e45673258e1420a2b1f1aee25a88c97d2
Syddraf88 <acid47132@gmail.com>
2025-07-18 00:28:07 -0500

    refactored for flexibility

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-def get_book_text(file_path):
+BOOK_DIR_PATH = "../bookbot/books/"
+
+def get_book_text(book_name):
     #file_path = "/home/matt/projects/github/Syddraf88/bookbot/bookbot/books/frankenstein.txt"
+    file_path = BOOK_DIR_PATH + book_name
     with open(file_path) as f:
         contents = f.read()
         return contents
 
+""" def word_count(file_path):
+    with open(file_path) as f:
+        book = f.read()
+        word_list = book.split()
+        count = len(word_list)
+        return f"{count} words found in the document" """
+
+def word_count(book_name):
+    book_text = get_book_text(book_name)
+    words = book_text.split()
+    return len(words)
+    
+    
+
+
 def main():
-    print(get_book_text("../bookbot/books/frankenstein.txt"))
+    #get_book_text("../bookbot/books/frankenstein.txt")
+    book_to_analyze = "frankenstein.txt"
+    num_words_found = word_count(book_to_analyze)
+    print(F"{num_words_found} words found in the document")
+
 
 main()
