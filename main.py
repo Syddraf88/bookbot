acace6cdd0e50d064d9131ed66f3320cf2ca480d
Syddraf88 <acid47132@gmail.com>
2025-07-19 13:31:15 -0500

    starting reports works

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ to use these functions you must define the book_to_analyze variable in the main 
 and the book must be stored in a relative path that matches "../bookbot/books/"
 '''
 
-from stats import word_count, char_count
+from stats import word_count, char_count_report
 
 def main():
     
     book_to_analyze = "frankenstein.txt"
     num_words_found = word_count(book_to_analyze)
-    character_count = char_count(book_to_analyze)
-    print(F"{num_words_found} words found in the document")
+    character_count = char_count_report(book_to_analyze)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/{book_to_analyze}...")
+    print("----------- Word Count ----------")
+    print(f"{num_words_found} words found in the document")
+    print("--------- Character Count -------")
     print(f"{character_count}")
 
 
--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@
 this file contains all of the functions related to data analasys of a selected book
 
 '''
+book_name = "frankenstein.txt"
 BOOK_DIR_PATH = "../bookbot/books/" #Global Variable to books folder
 def get_book_text(book_name): #Book file handler, Reads contents to ""
     
@@ -25,3 +26,20 @@ def char_count(book_name): #Counts total occurances of each character in the fil
         else:
             characters[character] += 1
     return characters
+
+def sort_on(d):
+    return d["num"]
+
+def char_count_report(book_name): #Refines character count into human readablie report of alpha num list.
+    report_data = char_count(book_name)
+    sorted_data = []
+    scrubbed_data = []
+    for ch in report_data:
+        scrubbed_data.append({"char": ch, "num": report_data[ch]})
+    sorted_data.sort(reverse=True, key=sort_on)
+    for item in sorted_data:
+        if item["char"].isalpha():
+            scrubbed_data.append
+
+            
+    return scrubbed_data
